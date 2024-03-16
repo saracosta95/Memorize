216c0ee67a1fd36ae8c8f48c8760aa5d1666dbe0
saracosta95 <saramcosta95@gmail.com>
2024-03-16 20:26:35 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sara Costa on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
